93eef41365b3325fd3f49dbed047599f9da4eed3
imgker <www@imgker.com>
2022-02-18 17:44:01 +0800

    fix

--- gitlab-ci/init.js
+++ gitlab-ci/init.js
@@ -6,7 +6,7 @@ request(`http://www.imgker.com/dbline/site/get_site_info?domain=${app_domain}`, 
         let app = JSON.parse(body)
         let code = String(1 + parseInt(app.header.title.split('.').join(''))).split('').join('.')
         let file_name = app.id + code
-        request(`http://www.imgker.com/dbline/article/create?title=${code}&hint=${process.env.NODE_ENV}&url=http://oss.imgker.com/file_name/dist/index.html&site=${app.id}&user_id=GITLAB`, function (error, response, body) {
+        request(`http://www.imgker.com/dbline/article/create?title=${code}&hint=${process.env.NODE_ENV}&url=http://oss.imgker.com/${file_name}/dist/index.html&site=${app.id}&user_id=GITLAB`, function (error, response, body) {
         });
         console.log(file_name) // 请求成功的处理逻辑
     }
